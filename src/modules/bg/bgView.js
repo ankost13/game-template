e38d9b671a59e481fd015588fddeb6aac5937aa2
Anastasiyka <anastasyam1908@gmail.com>
2025-03-13 22:51:30 +0200

    add Bg

---
+++ src/modules/bg/bgView.js
@@ -0,0 +1,10 @@
+import {View} from "../../utils/view";
+
+import gsap from 'gsap';
+
+export class BgView extends View {
+    constructor(parent, resizeData) {
+        super(parent, resizeData);
+    }
+
+}
